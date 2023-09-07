8cff0eb56a6c97ab44c956ee821d723b2c32afb7
gysim <qysim@naver.com>
2023-09-07 13:45:54 +0900

    230907

---
+++ gysim/0907_5658_보물상자비밀번호.py
@@ -0,0 +1,9 @@
+T = int(input())
+for tc in range(1, T+1):
+    N, K = map(int, input().split())
+    data = input()
+
+    # 중복제거
+    # 가능한 경우를 리스트에 다 담을건데 not in 이면 append하기기
+
+   print(f'#{tc}')
