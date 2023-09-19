001353132e7cb0c6ac031ce6e624891a5a4f19b3
icefoxand <such159357@naver.com>
2023-09-20 00:16:47 +0900

    .

---
+++ jwchoi/230919_공통조상/230919_공통조상.py
@@ -0,0 +1,5 @@
+# 최소 공통 조상 (LCA, Lowest Common Ancestor)
+# 최소 공통 조상은 트리 구조에서 임의의 두 정점이 갖는 가장 가까운 조상 정점을 의미한다.
+# 문제: 선택정점 두개의 level이 다르다면?
+# https://konkukcodekat.tistory.com/entry/%EB%B0%B1%EC%A4%80-11437-%EC%B5%9C%EC%86%8C-%EA%B3%B5%ED%86%B5-%EC%A1%B0%EC%83%81LCA-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%EA%B8%B0%EB%B3%B8-%ED%8C%8C%EC%9D%B4%EC%8D%ACPython
+# https://4legs-study.tistory.com/121
