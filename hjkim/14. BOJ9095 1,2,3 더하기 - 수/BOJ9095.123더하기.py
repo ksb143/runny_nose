e97291c2c6e70d3d946f652f16a412366e1aa13c
mok03189@naver.com <mok03189@naver.com>
2023-09-20 15:03:59 +0900

    123DPhyoju

---
+++ hjkim/14. BOJ9095 1,2,3 더하기 - 수/BOJ9095.123더하기.py
@@ -0,0 +1,14 @@
+T = int(input())
+for tc in range(T):
+    N = int(input())
+    dp = [0] * (N+1)
+    for i in range(1,N+1):
+        if i == 1:
+            dp[i] = 1
+        elif i == 2:
+            dp[i] = 2
+        elif i == 3:
+            dp[i] = 4
+        else:
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    print(dp[N])
