a5fe0bfde75bf7a960d88b0129048e95429ef661
mok03189@naver.com <mok03189@naver.com>
2023-09-21 17:59:29 +0900

    .

--- jwchoi/A형대비_황찬준/1_토마토/유사_2178미로탐색.py
+++ jwchoi/A형대비_황찬준/1_토마토/유사_2178미로탐색.py
@@ -1,23 +1,25 @@
 # https://uni2237.tistory.com/116
 # https://www.acmicpc.net/problem/2178
 from collections import deque
-n,m = map(int,input().split())
-data = [] # ['101111', '101010', '101011', '111011']
-for _ in range(n+1):
-    data.append(['0'] + list(input()))
-vi = [['0']*(m+1) for _ in range(n+1)]
-dir_idx = [[-1,0],[0,1],[1,0],[0,-1]]
 d = deque()
-print(data)
-print(d)
-print(vi)
-while d:
-    for i in range(1,n+1):
-        for j in range(1,m+1):
-            # data[i][j]
-            for k in range(4):
-                ni = i + dir_idx[k][0]
-                nj = j + dir_idx[k][1]
-                if 0<= ni <n+1 and 0 <= nj < m+1:
-                    if data[ni][nj] == '1':
-                        pass
+d.append((0,0))
+
+n,m = map(int,input().split())
+maze = [list(map(int,input().rstrip())) for _ in range(n)]
+
+vi = [[-1]*m for _ in range(n)]
+vi[0][0] = 1
+
+dir_idx = [[-1,0],[0,1],[1,0],[0,-1]]
+
+while d: # deque 가 비지 않은 동안 작동
+    x,y = d.popleft() # (0,0)에서
+    for k in range(4):
+        nx = x + dir_idx[k][0]
+        ny = y + dir_idx[k][1]
+        if 0<=nx<n and 0<=ny<m and vi[nx][ny]==-1: # 4방향에 방문안한 1이 있다면
+            if maze[nx][ny] ==1:
+                d.append((nx,ny))
+                vi[nx][ny] = vi[x][y]+1
+                # vi
+print(vi[n-1][m-1])
