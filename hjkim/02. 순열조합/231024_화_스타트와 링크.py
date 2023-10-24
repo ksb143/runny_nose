c756690114ea7b4e24e3af53283c04e6c7c827a6
HyojuKim <mok03189@naver.com>
2023-10-24 17:51:57 +0900

    .

--- hjkim/02. 순열조합/231024_화_스타트와 링크.py
+++ hjkim/02. 순열조합/231024_화_스타트와 링크.py
@@ -1,47 +1,41 @@
-# total_people : 짝수
-# N / 2 명 으로 teamA, teamB 나눔
-# teamA, teamB 간 능력치 최소화하고 차를 출력
-def comb(start, cnt):
+# 조합 : 전체에서 주어진 개수만큼 선택
+# 각 요소가 조합에 포함되는지 아닌지 결정하는 것은 동일
+# 포함되는 요소의 개수가 정해져 있는것이 다르다..
+# cnt : 여태까지 몇개의 요소가 선택되었는지 알려주는 변수
+def comb(idx, cnt):
 
-    # teamA 다 골랐으면 탐색 중단하고 나머지는 teamB 로 보내기
-    if cnt == pick_num:
+    # 내가 필요한 만큼 다 골랐다
+    if cnt == M:
+        #여기가 성공케이스
+        for el in range(4):
+
         return
 
-    # Index Error 방지
-    if start == total_people:
+    # 모든 요소에 대해서 조합 포함여부 결정
+    # 여기는 마지막 인덱스 까지 결정했는데..
+    # 필요한 개수만큼 선택하지 못함
+    if idx == N:
         return
 
-    # person : start 부터 마지막까지 번호 사람들 중에 고를 사람의 번호
-    for person in range(start, total_people):
-        selected[person] = 1
+    # idx 번째 요소 고르기
+    selected[idx] = 1
+    comb(idx+1, cnt + 1)
+    # idx 번째 요소 안 고르기
+    selected[idx] = 0
+    comb(idx+1, cnt)
 
+# 전체 사람 수
+N = int(input())
+people = [i for i in range(1, N + 1)]
+# 한 팀 당 사람 수
+M = N // 2
+#각 요소 포함여부 표시배열
+selected = [0] * N
+groups = []
+g1 = []
+g2 = []
 
-
-total_people = int(input())
-power = [list(map(int, input().split())) for _ in range(total_people)]
-# power[r][c] 랑 power[c][r] 은 다름
-
-# 풀이 방법
-# 순열과 조합
-# pick_num = 팀 멤버 수
-pick_num = total_people // 2
-selected = [-1] * pick_num
-power_a = 0
-power_b = 0
-power_a_lst = []
-power_b_lst = []
+# idx : people 내부 도는 idx
+# cnt : 선택된 요소의 개수
+# comb(idx,cnt)
 comb(0, 0)
-
-
-
-
-
-
-
-
-
-
-
-
-
-# 서로소집합 활용 (UNION - FIND)
