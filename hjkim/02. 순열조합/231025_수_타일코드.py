0ee583c406ede19d050219ccb0d54f2a432090ba
HyojuKim <mok03189@naver.com>
2023-10-25 17:58:14 +0900

    .

---
+++ hjkim/02. 순열조합/231025_수_타일코드.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+blocks = {
+    'a' : 1,
+    'b' : 2,
+    'c' : 2,
+}
+
+
+'''
+N = 1 'a'
+N = 2 'aa', 'b', 'c'
+N = 3 'aaa', 'ab', 'ac'
+N = 4 'aaaa', 'aba', 'aca', 'aab', 'aac', 'bb', 'bc', 'cc'
+'''
