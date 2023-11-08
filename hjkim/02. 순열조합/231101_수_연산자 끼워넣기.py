72ea9605d8fbc940cb2c143b88880ac9a318d961
HyojuKim <mok03189@naver.com>
2023-11-08 17:29:08 +0900

    .

--- hjkim/02. 순열조합/231101_수_연산자 끼워넣기.py
+++ hjkim/02. 순열조합/231101_수_연산자 끼워넣기.py
@@ -1,46 +1,19 @@
 from collections import deque
 
-# selected 의 idx 번째를 선택
-
-
-def comb(idx, pick_num):
-    if idx == pick_num:
-        op_position.append(selected)
-        # print(op_position)
-        return
-
-    for op in range(pick_num):
-        if not visited[op]:
-            selected[idx] = operators_full[op]
-            visited[op] = 1
-            comb(idx + 1, pick_num)
-            visited[op] = 0
-
-
-N = int(input())
-
-numbers = deque(map(int, input().split()))
-
+N = int(input())                              # 수의 개수
+# N = 2
+numbers = deque(map(int, input().split()))     # 수열
+# numbers = [5, 6]
 operators_num = deque(map(int, input().split()))
-
+# calc_num = [0,0,1,0]
 operators = ['+', '-', '*', '//']
 operators_full = deque()
 for el in range(4):
     if operators_num[el]:
         operators_full.append(operators[el] * operators_num[el])
-# print(operators_full)
-
-M = len(operators_full)
-# print(M)
-
-selected = [0] * M
-op_position = []
-visited = [0] * M
-comb(0, M)
-
-
-# print(visited)
-
+# operators = ['*']
+M = len(operators) # 1
+visited = [0] * M # [0]
 results = []
 
 val = numbers[0]
@@ -70,6 +43,5 @@ while numbers:
     results.append(val)
 
 # 마지막에 돌면서 최대 최소 구하기
-# print(results)
-# print(max(results))
-# print(min(results))
+print(max(results))
+print(min(results))
