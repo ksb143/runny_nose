c12fc9d77848438907f02600f51aad9bcffa5db5
김수빈 <ksb143@naver.com>
2024-01-26 23:54:46 +0900

    타겟 넘버 (김수빈)

--- sbkim/PROGRAMMERS/240126_타겟_넘버.py
+++ sbkim/PROGRAMMERS/240126_타겟_넘버.py
@@ -2,6 +2,11 @@
 # 이 정수들을 순서를 바꾸지 않고 적절히 더하거나 빼서 타겟 넘버를 만들려고 합니다.
 # 예를 들어 [1, 1, 1, 1, 1]로 숫자 3을 만들려면
 # 다음 다섯 방법을 쓸 수 있습니다.
+# -1+1+1+1+1 = 3
+# +1-1+1+1+1 = 3
+# +1+1-1+1+1 = 3
+# +1+1+1-1+1 = 3
+# +1+1+1+1-1 = 3
 
 # 사용할 수 있는 숫자가 담긴 배열 numbers,
 # 타겟 넘버 target이 매개변수로 주어질 때
@@ -12,7 +17,35 @@
 # 주어지는 숫자의 개수는 2개 이상 20개 이하입니다.
 # 각 숫자는 1 이상 50 이하인 자연수입니다.
 # 타겟 넘버는 1 이상 1000 이하인 자연수입니다.
+from collections import deque
+
+
 def solution(numbers, target):
-    numbers
-    answer = 0
-    return answer
+    # 길이
+    N = len(numbers)
+
+    # 초기상태 만들기
+    queue = deque()
+    queue.append((0, 0))
+
+    # 타겟 넘버로 만들어진 카운트
+    cnt = 0
+
+    while queue:
+        # num, idx 꺼내기
+        num, idx = queue.pop()
+        # idx가 N이고 그게 타겟 넘버와 같으면 cnt 증가 후 continue
+        if idx == N and num == target:
+            cnt += 1
+            continue
+        # idx가 그냥 N이면 continue
+        elif idx == N:
+            continue
+        # 나머지 경우에는 숫자를 계속 삽입하기
+        queue.append((num + numbers[idx], idx + 1))
+        queue.append((num - numbers[idx], idx + 1))
+
+    return cnt
+
+
+solution([4, 1, 2, 1], 2)
